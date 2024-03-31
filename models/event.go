b0e6a3a04006ac7cf1a5d71bbec7b10c7277c296
kortby@gmail.com <benk@Bens-MBP.lan>
2024-03-31 13:42:50 -0700

    first commit

---
+++ models/event.go
@@ -0,0 +1,65 @@
+package models
+
+import (
+	"time"
+	"example/gingonic/db"
+)
+
+type Event struct {
+    ID          int64     `json:"id"`
+    Name        string    `json:"name" binding:"required"`
+    Description string    `json:"description" binding:"required"`
+    Location    string    `json:"location" binding:"required"`
+    DateTime    time.Time `json:"datetime" binding:"required"`
+    UserID      int       `json:"user_id"`
+}
+
+
+func (e *Event) Save() error {
+    query := `INSERT INTO events(name, description, location, datetime, user_id)
+              VALUES (?, ?, ?, ?, ?)`
+    sqlStatement, err := db.DB.Prepare(query)
+    if err != nil {
+        return err
+    }
+    defer sqlStatement.Close()
+
+	ParsedDatetime := e.DateTime.Format("2006-01-02 15:04:05")
+
+    result, err := sqlStatement.Exec(e.Name, e.Description, e.Location, ParsedDatetime, e.UserID)
+    if err != nil {
+        return err
+    }
+
+    id, err := result.LastInsertId()
+    if err != nil {
+        return err
+    }
+
+    e.ID = id
+    return nil
+}
+
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+	
+	return events, nil
+}
